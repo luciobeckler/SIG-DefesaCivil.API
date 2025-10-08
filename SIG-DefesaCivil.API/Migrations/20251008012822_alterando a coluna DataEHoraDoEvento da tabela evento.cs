7736ed9e4ba78358856beaace15fba4a2e9ef456
luciobeckler <luciobecklerp@gmail.com>
2025-10-07 22:29:03 -0300

    fix: corrigindo nome da coluna que estava desatualizado no banco

---
+++ SIG-DefesaCivil.API/Migrations/20251008012822_alterando a coluna DataEHoraDoEvento da tabela evento.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIG_DefesaCivil.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class alterandoacolunaDataEHoraDoEventodatabelaevento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataHora",
+                table: "Eventos",
+                newName: "DataEHoraDoEvento");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DataEHoraDoEvento",
+                table: "Eventos",
+                newName: "DataHora");
+        }
+    }
+}
