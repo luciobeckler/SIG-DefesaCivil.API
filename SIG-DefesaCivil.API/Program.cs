9c9cbf70ec3a7c9b2bed0fb9c2e7685d4e5e288a
luciobeckler <luciobecklerp@gmail.com>
2026-02-03 08:23:36 -0300

    fix: adicionando aplicação de migrations automática

--- SIG-DefesaCivil.API/Program.cs
+++ SIG-DefesaCivil.API/Program.cs
@@ -237,4 +237,19 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DefesaCivilDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro ao aplicar as migrations.");
+    }
+}
+
 app.Run();
